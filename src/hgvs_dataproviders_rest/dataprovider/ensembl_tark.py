4efad21fd2df5d7c7c5065c67dec354d7e3dd1e2
Dave Lawrence <davmlaw@gmail.com>
2025-09-05 15:24:24 -0400

    Comment

--- src/hgvs_dataproviders_rest/dataprovider/ensembl_tark.py
+++ src/hgvs_dataproviders_rest/dataprovider/ensembl_tark.py
@@ -5,5 +5,6 @@ from src.hgvs_dataproviders_rest.txdata.txdata_ensembl_tark import EnsemblTarkDa
 def connect():
     tx_data_provider = EnsemblTarkDataProvider()
     ensembl_tark_seqfetcher = EnsemblTarkSeqFetcher()
+    # EnsemblTark returns Transcripts and Sequences - so we need to hook them together
     ensembl_tark_seqfetcher.set_data_provider(tx_data_provider)
 
